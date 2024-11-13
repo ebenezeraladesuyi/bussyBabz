a01e97ac69afd845f0ff8f675ba8e5f925d4a446
ebenezeraladesuyi <ebenezeraladesuyi@gmail.com>
2024-11-13 16:00:27 +0100

    new-server

--- src/utils/Api.tsx
+++ src/utils/Api.tsx
@@ -6,4 +6,4 @@ export const url2 = "https://volaticreg-be.onrender.com"
 
 // export const url3 = "http://localhost:2024"
 
-export const url3 = "https://election-server-0ele.onrender.com"
+export const url3 = "https://election-server-1.onrender.com"
