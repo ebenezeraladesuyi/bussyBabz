e8808b1d42c273c36c79255e0b09c369cd24731d
ebenezeraladesuyi <ebenezeraladesuyi@gmail.com>
2024-11-13 11:19:42 +0100

    new

--- src/components/static/ElectionCard.tsx
+++ src/components/static/ElectionCard.tsx
@@ -4,14 +4,17 @@ import React from 'react'
 // import { MdClose } from "react-icons/md";
 // import lady from "../../assets/images/lady-2.jpg";
 
+// import apc from "../../assets/images/apc.jpg";
+
 interface iCard {
     image: string;
+    logo: string;
     name: string;
     brief: string;
 }
 
 
-const ElectionCard:React.FC<iCard> = ({image, name, brief}) => {
+const ElectionCard:React.FC<iCard> = ({image, name, brief, logo}) => {
 
     // const [open, setOpen] = React.useState(false)
 
@@ -20,13 +23,14 @@ const ElectionCard:React.FC<iCard> = ({image, name, brief}) => {
     // }
 
   return (
-    <div className="w-[300px]  lg:w-[24%] p-[10px]">
+    <div className="w-[300px]  lg:w-[24%] p-[10px] text-white">
         <img className="w-full h-[280px] rounded-[10px]" src={image} alt="" />
 
         <div className="mt-[10px] flex justify-between items-center border-[1px] border-gray-400 p-2 rounded-md">
             <h1 className="text-[15px] md:text-[17px] font-bold">
                 {/* HON. LUCY AYEDAMIDATHA */}
                 {name}
+                <img className="w-[50px] h-[50px] md:w-[70px] md:h-[70px] rounded-full overflow-hidden mt-[5px]" src={logo} alt="" />
             </h1>
 
             {/* { open ? 
